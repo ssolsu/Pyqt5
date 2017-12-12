2b4cd71f75e48c645efba8d4244c0f055100774a
eathan <ssolsu1@126.com>
2017-12-12 23:59:21 +0800

    12/12

--- main_win.py
+++ main_win.py
@@ -3,6 +3,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QGridLayout, QVBoxLayout, QLineEdit
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 import sys
 import urllib.request
 import json
@@ -11,6 +12,7 @@ import json
 class main_win(QWidget):
     def __init__(self):
         super(main_win, self).__init__()
+        self.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint)
         self.initui()
 
     def initui(self):
@@ -28,18 +30,24 @@ class main_win(QWidget):
         self.label2_1 = QLabel()
         self.label3 = QLabel('英式发音：')
         self.label3_1 = QLabel()
-        self.gl.addWidget(self.label1, 1, 0)
-        self.gl.addWidget(self.ledit, 1, 1)
-        self.gl.addWidget(self.label2, 2, 0)
-        self.gl.addWidget(self.label2_1, 2, 1)
-        self.gl.addWidget(self.label3, 2, 2)
-        self.gl.addWidget(self.label3_1, 2, 3)
+        self.label4 = QLabel('翻译区域：')
+        self.label4_1 = QLabel()
+        # self.label4_1.setGeometry(QRect(80, 5, 20, 20))
+        self.label4_1.setWordWrap(True)
+        self.gl.addWidget(self.label1, 0, 0)
+        self.gl.addWidget(self.ledit, 0, 1, 1, 2)
+        self.gl.addWidget(self.label2, 1, 0)
+        self.gl.addWidget(self.label2_1, 1, 1)
+        self.gl.addWidget(self.label3, 1, 2)
+        self.gl.addWidget(self.label3_1, 1, 3)
+        self.gl.addWidget(self.label4, 2, 0, 4, 1)
+        self.gl.addWidget(self.label4_1, 2, 1, 4, 3)
         # 把gl布局添加到主布局中
         self.wvl.addLayout(self.gl)
         # 设置窗体布局为主布局
         self.setLayout(self.wvl)
-        self.setGeometry(0, 0, 300, 100)
-        self.setStyleSheet("QLabel{color:rgb(0,0,0,250);font-size:9px;font-family:Roman times;}"
+        self.setGeometry(0, 0, 300, 200)
+        self.setStyleSheet("QLabel{color:rgb(0,0,0,250);font-size:10px;font-family:Roman times;}"
                            "QLabel:hover{color:red;}")
         self.show()
 
@@ -54,16 +62,14 @@ class main_win(QWidget):
             self.label2_1.setText(jsondata['pronunciation']['AmE'])
             self.label3_1.setText(jsondata['pronunciation']['BrE'])
 
-
-class tran():
-    def request_data(self):
-        print('fuck')
-        req = urllib.request.Request('http://xtk.azurewebsites.net/BingDictService.aspx?Word=welcome')
-        with urllib.request.urlopen(req) as response:
-            the_page = response.read().decode('utf-8')
-        jsondata = json.loads(the_page)
-        print(jsondata)
-        return jsondata
+        else:
+            self.label2_1.setText('')
+            self.label3_1.setText('')
+        tran_text = ""
+        for i in jsondata['defs']:
+            # print(i['pos'], i['def'])
+            tran_text += i['pos'] + i['def'] + '\n'
+        self.label4_1.setText(tran_text)
 
 
 if __name__ == '__main__':
