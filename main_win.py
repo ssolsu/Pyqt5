2352e556706aa3106b6c4b1ff373c73a56eebbaf
eathan <ssolsu1@126.com>
2017-12-21 00:03:38 +0800

    12/20

--- main_win.py
+++ main_win.py
@@ -4,7 +4,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QGridLayout, QVB
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
-import sys
+from bs4 import BeautifulSoup
+import sys, re
 import urllib.request
 import json
 
@@ -12,7 +13,8 @@ import json
 class main_win(QWidget):
     def __init__(self):
         super(main_win, self).__init__()
-        self.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint|Qt.MSWindowsFixedSizeDialogHint )
+
         self.initui()
 
     def initui(self):
@@ -24,52 +26,63 @@ class main_win(QWidget):
         self.gl = QGridLayout()
         # 编辑gl布局
         self.ledit = QLineEdit()
-        self.ledit.returnPressed.connect(self.stran)
+        self.ledit.returnPressed.connect(self.stran1)
         self.label1 = QLabel('查找的单词')
-        self.label2 = QLabel('美式发音：')
+        self.label2 = QLabel('英式发音：')
         self.label2_1 = QLabel()
-        self.label3 = QLabel('英式发音：')
-        self.label3_1 = QLabel()
         self.label4 = QLabel('翻译区域：')
         self.label4_1 = QLabel()
+        self.label4_1.setStyleSheet('color:green')
         # self.label4_1.setGeometry(QRect(80, 5, 20, 20))
         self.label4_1.setWordWrap(True)
         self.gl.addWidget(self.label1, 0, 0)
-        self.gl.addWidget(self.ledit, 0, 1, 1, 2)
+        self.gl.addWidget(self.ledit, 0, 1, 1, 1)
         self.gl.addWidget(self.label2, 1, 0)
         self.gl.addWidget(self.label2_1, 1, 1)
-        self.gl.addWidget(self.label3, 1, 2)
-        self.gl.addWidget(self.label3_1, 1, 3)
-        self.gl.addWidget(self.label4, 2, 0, 4, 1)
-        self.gl.addWidget(self.label4_1, 2, 1, 4, 3)
+        self.gl.addWidget(self.label4_1, 2, 0, 4, 2)
         # 把gl布局添加到主布局中
         self.wvl.addLayout(self.gl)
         # 设置窗体布局为主布局
         self.setLayout(self.wvl)
-        self.setGeometry(0, 0, 300, 200)
+        self.setGeometry(0, 0, 200, 200)
         self.setStyleSheet("QLabel{color:rgb(0,0,0,250);font-size:10px;font-family:Roman times;}"
                            "QLabel:hover{color:red;}")
         self.show()
 
-    def stran(self):
+    def stran1(self):
         self.word = urllib.request.quote(self.ledit.text())
-        req = urllib.request.Request('http://xtk.azurewebsites.net/BingDictService.aspx?Word=%s' % self.word)
+        req = urllib.request.Request(
+            'http://cn.bing.com/dict/search?q=%s&qs=n&form=Z9LH5&sp=-1&pq=%s&sc=3-12&sk=&cvid=BE4372C421374C658F9CC629DF84F6E9' % (
+                self.word, self.word))
         with urllib.request.urlopen(req) as response:
-            the_page = response.read().decode('utf-8')
-        print(the_page)
-        jsondata = json.loads(the_page)
-        if jsondata['pronunciation'] != None:
-            self.label2_1.setText(jsondata['pronunciation']['AmE'])
-            self.label3_1.setText(jsondata['pronunciation']['BrE'])
+            the_page = response.read()
+        self.soup = BeautifulSoup(the_page, "lxml")
+        dict_stran = {'Ame': ''}
+        try:
+            tags = self.soup.find('div', id="headword")
+            for tag in tags:
+                if tag.get_text() != None:
+                    dict_stran['word'] = tag.get_text()
+            tags = self.soup.find_all('div', class_="hd_pr")
+            for tag in tags:
+                if tag.get_text() != None:
+                    dict_stran['Ame'] = tag.get_text()
+        except TypeError as e:
+            print(e)
+            self.label2_1.setText('没有读音')
+        t2 = self.soup.find_all('li')
+        yisi = ''
+        for i in t2:
+            if i.find(class_='pos') != None:
+                yisi = yisi + i.get_text() + "\n"
+                # print(i,i.name,i.attrs,i.find(class_='pos'))
 
-        else:
-            self.label2_1.setText('')
-            self.label3_1.setText('')
-        tran_text = ""
-        for i in jsondata['defs']:
-            # print(i['pos'], i['def'])
-            tran_text += i['pos'] + i['def'] + '\n'
-        self.label4_1.setText(tran_text)
+        self.label2_1.setText(dict_stran['Ame'])
+        self.label4_1.setText(yisi)
+
+    def del_content_blank(s):
+        clean_str = re.sub(r'\n|&nbsp|\xa0|\\xa0|\u3000|\\u3000|\\u0020|\u0020', '', str(s))
+        return clean_str
 
 
 if __name__ == '__main__':
